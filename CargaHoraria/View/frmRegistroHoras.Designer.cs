062465f0bd923abbf83491807d2181872e3c86c8
rodrigo silva <rodrigmars@gmail.com>
2015-09-30 22:23:26 -0300

    primeiro commit

---
+++ CargaHoraria/View/frmRegistroHoras.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace CargaHoraria.View
+{
+    partial class frmRegistroHoras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHraEntradaT1 = new System.Windows.Forms.Label();
+            this.lblHraSaidaT1 = new System.Windows.Forms.Label();
+            this.txtHraEntradaT1 = new System.Windows.Forms.TextBox();
+            this.txtHraSaidaT1 = new System.Windows.Forms.TextBox();
+            this.lblHraEntradaT2 = new System.Windows.Forms.Label();
+            this.txtHraEntradaT2 = new System.Windows.Forms.TextBox();
+            this.txtHraSaidaT2 = new System.Windows.Forms.TextBox();
+            this.lblHraSaidaT2 = new System.Windows.Forms.Label();
+            this.gbHraTurno1 = new System.Windows.Forms.GroupBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lblTotalHoras = new System.Windows.Forms.Label();
+            this.txtTotalHoras = new System.Windows.Forms.RichTextBox();
+            this.gbHraTurno2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gbHraTurno1.SuspendLayout();
+            this.gbHraTurno2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHraEntradaT1
+            // 
+            this.lblHraEntradaT1.AutoSize = true;
+            this.lblHraEntradaT1.Location = new System.Drawing.Point(58, 47);
+            this.lblHraEntradaT1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblHraEntradaT1.Name = "lblHraEntradaT1";
+            this.lblHraEntradaT1.Size = new System.Drawing.Size(75, 22);
+            this.lblHraEntradaT1.TabIndex = 0;
+            this.lblHraEntradaT1.Text = "Entrada";
+            // 
+            // lblHraSaidaT1
+            // 
+            this.lblHraSaidaT1.AutoSize = true;
+            this.lblHraSaidaT1.Location = new System.Drawing.Point(185, 47);
+            this.lblHraSaidaT1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblHraSaidaT1.Name = "lblHraSaidaT1";
+            this.lblHraSaidaT1.Size = new System.Drawing.Size(60, 22);
+            this.lblHraSaidaT1.TabIndex = 2;
+            this.lblHraSaidaT1.Text = "Saída";
+            // 
+            // txtHraEntradaT1
+            // 
+            this.txtHraEntradaT1.ForeColor = System.Drawing.Color.Blue;
+            this.txtHraEntradaT1.Location = new System.Drawing.Point(35, 74);
+            this.txtHraEntradaT1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtHraEntradaT1.Name = "txtHraEntradaT1";
+            this.txtHraEntradaT1.Size = new System.Drawing.Size(98, 29);
+            this.txtHraEntradaT1.TabIndex = 3;
+            this.txtHraEntradaT1.Text = "00:00";
+            this.txtHraEntradaT1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtHraSaidaT1
+            // 
+            this.txtHraSaidaT1.ForeColor = System.Drawing.Color.Blue;
+            this.txtHraSaidaT1.Location = new System.Drawing.Point(147, 74);
+            this.txtHraSaidaT1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtHraSaidaT1.Name = "txtHraSaidaT1";
+            this.txtHraSaidaT1.Size = new System.Drawing.Size(98, 29);
+            this.txtHraSaidaT1.TabIndex = 4;
+            this.txtHraSaidaT1.Text = "00:00";
+            this.txtHraSaidaT1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblHraEntradaT2
+            // 
+            this.lblHraEntradaT2.AutoSize = true;
+            this.lblHraEntradaT2.Location = new System.Drawing.Point(66, 44);
+            this.lblHraEntradaT2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblHraEntradaT2.Name = "lblHraEntradaT2";
+            this.lblHraEntradaT2.Size = new System.Drawing.Size(75, 22);
+            this.lblHraEntradaT2.TabIndex = 5;
+            this.lblHraEntradaT2.Text = "Entrada";
+            // 
+            // txtHraEntradaT2
+            // 
+            this.txtHraEntradaT2.ForeColor = System.Drawing.Color.Blue;
+            this.txtHraEntradaT2.Location = new System.Drawing.Point(47, 71);
+            this.txtHraEntradaT2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtHraEntradaT2.Name = "txtHraEntradaT2";
+            this.txtHraEntradaT2.Size = new System.Drawing.Size(98, 29);
+            this.txtHraEntradaT2.TabIndex = 6;
+            this.txtHraEntradaT2.Text = "00:00";
+            this.txtHraEntradaT2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtHraSaidaT2
+            // 
+            this.txtHraSaidaT2.ForeColor = System.Drawing.Color.Blue;
+            this.txtHraSaidaT2.Location = new System.Drawing.Point(158, 68);
+            this.txtHraSaidaT2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtHraSaidaT2.Name = "txtHraSaidaT2";
+            this.txtHraSaidaT2.Size = new System.Drawing.Size(98, 29);
+            this.txtHraSaidaT2.TabIndex = 7;
+            this.txtHraSaidaT2.Text = "00:00";
+            this.txtHraSaidaT2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblHraSaidaT2
+            // 
+            this.lblHraSaidaT2.AutoSize = true;
+            this.lblHraSaidaT2.Location = new System.Drawing.Point(196, 43);
+            this.lblHraSaidaT2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblHraSaidaT2.Name = "lblHraSaidaT2";
+            this.lblHraSaidaT2.Size = new System.Drawing.Size(60, 22);
+            this.lblHraSaidaT2.TabIndex = 8;
+            this.lblHraSaidaT2.Text = "Saída";
+            // 
+            // gbHraTurno1
+            // 
+            this.gbHraTurno1.Controls.Add(this.txtHraSaidaT1);
+            this.gbHraTurno1.Controls.Add(this.txtHraEntradaT1);
+            this.gbHraTurno1.Controls.Add(this.lblHraSaidaT1);
+            this.gbHraTurno1.Controls.Add(this.lblHraEntradaT1);
+            this.gbHraTurno1.Location = new System.Drawing.Point(22, 20);
+            this.gbHraTurno1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.gbHraTurno1.Name = "gbHraTurno1";
+            this.gbHraTurno1.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.gbHraTurno1.Size = new System.Drawing.Size(265, 149);
+            this.gbHraTurno1.TabIndex = 9;
+            this.gbHraTurno1.TabStop = false;
+            this.gbHraTurno1.Text = "Turno 1º";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfirmar.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnConfirmar.Location = new System.Drawing.Point(612, 132);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(102, 37);
+            this.btnConfirmar.TabIndex = 10;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // lblTotalHoras
+            // 
+            this.lblTotalHoras.AutoSize = true;
+            this.lblTotalHoras.Location = new System.Drawing.Point(609, 61);
+            this.lblTotalHoras.Name = "lblTotalHoras";
+            this.lblTotalHoras.Size = new System.Drawing.Size(105, 22);
+            this.lblTotalHoras.TabIndex = 9;
+            this.lblTotalHoras.Text = "Total Horas";
+            // 
+            // txtTotalHoras
+            // 
+            this.txtTotalHoras.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.txtTotalHoras.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTotalHoras.ForeColor = System.Drawing.Color.Green;
+            this.txtTotalHoras.Location = new System.Drawing.Point(612, 86);
+            this.txtTotalHoras.Name = "txtTotalHoras";
+            this.txtTotalHoras.ReadOnly = true;
+            this.txtTotalHoras.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtTotalHoras.Size = new System.Drawing.Size(100, 31);
+            this.txtTotalHoras.TabIndex = 10;
+            this.txtTotalHoras.Text = "00:00";
+            // 
+            // gbHraTurno2
+            // 
+            this.gbHraTurno2.Controls.Add(this.txtHraEntradaT2);
+            this.gbHraTurno2.Controls.Add(this.txtHraSaidaT2);
+            this.gbHraTurno2.Controls.Add(this.lblHraEntradaT2);
+            this.gbHraTurno2.Controls.Add(this.lblHraSaidaT2);
+            this.gbHraTurno2.Location = new System.Drawing.Point(297, 20);
+            this.gbHraTurno2.Name = "gbHraTurno2";
+            this.gbHraTurno2.Size = new System.Drawing.Size(265, 149);
+            this.gbHraTurno2.TabIndex = 11;
+            this.gbHraTurno2.TabStop = false;
+            this.gbHraTurno2.Text = "Turno 2º";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 178);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(702, 270);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // frmRegistroHoras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 460);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.gbHraTurno2);
+            this.Controls.Add(this.txtTotalHoras);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.lblTotalHoras);
+            this.Controls.Add(this.gbHraTurno1);
+            this.Font = new System.Drawing.Font("Arial", 14F);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "frmRegistroHoras";
+            this.Text = "REGISTRO DE HORAS";
+            this.gbHraTurno1.ResumeLayout(false);
+            this.gbHraTurno1.PerformLayout();
+            this.gbHraTurno2.ResumeLayout(false);
+            this.gbHraTurno2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHraEntradaT1;
+        private System.Windows.Forms.Label lblHraSaidaT1;
+        private System.Windows.Forms.TextBox txtHraEntradaT1;
+        private System.Windows.Forms.TextBox txtHraSaidaT1;
+        private System.Windows.Forms.Label lblHraEntradaT2;
+        private System.Windows.Forms.TextBox txtHraEntradaT2;
+        private System.Windows.Forms.TextBox txtHraSaidaT2;
+        private System.Windows.Forms.Label lblHraSaidaT2;
+        private System.Windows.Forms.GroupBox gbHraTurno1;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.RichTextBox txtTotalHoras;
+        private System.Windows.Forms.Label lblTotalHoras;
+        private System.Windows.Forms.GroupBox gbHraTurno2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
